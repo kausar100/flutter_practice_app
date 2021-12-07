062abea9b1b07bd3a45eb29bd74281d579541bca
kausar100 <kausar.cse16@gmail.com>
2021-12-07 23:36:50 +0600

    gridview added day10 completed

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
@@ -22,12 +20,14 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
-    await Future.delayed(Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 2));
     final catalogJson =
         await rootBundle.loadString("assets/files/catalog.json");
     final decodedData = jsonDecode(catalogJson);
     var productsData = decodedData["products"];
-    CatalogModel.items = List.from(productsData).map<Item>((item) => Item.fromMap(item)).toList();
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
     setState(() {});
   }
 
@@ -40,17 +40,32 @@ class _HomePageState extends State<HomePage> {
           'Catalog App',
         ),
       ),
-     body: Padding(
+      body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items.length,
-                itemBuilder: (context, index) => ItemWidget(
-                  item: CatalogModel.items[index],
+            ? GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 15,
+                  crossAxisSpacing: 15,
                 ),
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    child: GridTile(
+                      header: Text(item.name),
+                      child: Image.network(item.image),
+                      footer: Text("\$${item.price}"),
+                    ),
+                  );
+                },
               )
-            : Center(
-                child: CircularProgressIndicator(color: Colors.blue,),
+            : const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.blue,
+                ),
               ),
       ),
       drawer: const MyDrawer(),
